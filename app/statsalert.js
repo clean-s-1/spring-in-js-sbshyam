9ab02b2105282feb5abb0cd89aec805f35337a12
sbshyam <105294247+sbshyam@users.noreply.github.com>
2022-05-10 19:45:01 +0530

    Update statsalert.js

--- app/statsalert.js
+++ app/statsalert.js
@@ -7,7 +7,7 @@ module.exports= class StatsAlerter{
      }
      checkAndAlert(nums){
          var maxi= Math.max(...nums);
-         if(this.maxThreshold>maxi){
+         if(this.maxThreshold<maxi){
             this.alerters[0].emailSent=true;
             this.alerters[1].ledGlows=true;
          }
