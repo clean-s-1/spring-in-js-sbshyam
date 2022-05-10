18becc344ced8e47d3f499c240ed961851a2b863
sbshyam <105294247+sbshyam@users.noreply.github.com>
2022-05-10 19:23:06 +0530

    Update statistics.spec.js

--- spec/statistics.spec.js
+++ spec/statistics.spec.js
@@ -32,8 +32,8 @@ describe("StatisticsTest",function(){
          const statsAlerter=new StatsAlerter(maxThreshold, alerters);
          statsAlerter.checkAndAlert([99.8, 34.2, 4.5, 6.7]);
      
-         expect(emailAlert.emailSent).tobe(true);
-         expect(ledAlert.ledGlows).tobe(true);
+         expect(emailAlert.emailSent).toBe(true);
+         expect(ledAlert.ledGlows).toBe(true);
 
         
         
