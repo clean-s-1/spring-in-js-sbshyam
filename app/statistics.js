bccf8e0126d4bb67c755b1be3ecf351d7012fba2
sbshyam <105294247+sbshyam@users.noreply.github.com>
2022-05-10 19:21:49 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -7,11 +7,7 @@ module.exports=function getStatistics(numbers){
     stats.average=stats.average/numbers.length;
     stats.min=Math.min(...numbers);
     stats.max=Math.max(...numbers);
-    
-    return stats;
-    //implement the computation of statistics here
-}
- class EmailAlert{
+     class EmailAlert{
      emailSent=false;
      constructor(){
      }
@@ -36,3 +32,7 @@ module.exports=function getStatistics(numbers){
          }
      }
  }
+    return stats;
+    //implement the computation of statistics here
+}
+
