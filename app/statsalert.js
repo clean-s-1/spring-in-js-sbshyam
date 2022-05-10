793af6a936977315ce6c37d4d8794c43f8fc84a6
sbshyam <105294247+sbshyam@users.noreply.github.com>
2022-05-10 19:39:25 +0530

    Create statsalert.js

---
+++ app/statsalert.js
@@ -0,0 +1,15 @@
+module.exports= class StatsAlerter{
+     maxThreshold=0;
+     alerters=[];
+     constructor(maxThreshold,alerters){
+        this.maxThreshold=maxThreshold;
+        this.alerters=alerters;
+     }
+     checkAndAlert(nums){
+         var maxi= Math.max(...nums);
+         if(this.maxThreshold>maxi){
+            this.alerters[0].emailSent=true;
+            this.alerters[1].ledGlows=true;
+         }
+     }
+ }
